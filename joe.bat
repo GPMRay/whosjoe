7016fe443c0959b912d4df25e1c3b733f04c35d8
GPMRay <71682126+GPMRay@users.noreply.github.com>
2021-02-11 21:45:36 -0600

    Who Is Joe?!? (v9.51 RC2 V3.69.420 Pre-PostClosedAlpha 7 PublicBuild 4.42069)
    Made by Ray Designs Development Modification Studios INC LLC Escort © 2021 FTW #PrayForAfrica #PasswordIs"joe"

---
+++ joe.bat
@@ -0,0 +1,486 @@
+@echo off
+title Who Is Joe?!? (v9.51 RC2 V3.69.420 Pre-PostClosedAlpha 7 PublicBuild 4.42069) Made by Ray Designs Development Modification Studios INC LLC Escort © 2021 FTW #PrayForAfrica #PasswordIs"joe"
+:warning
+color a
+echo This "SOFTWARE" (lol) can cause epalep---fuck it idk how to spell it but you get what I mean. Proceed with caution.
+pause
+goto password
+
+:password
+color a
+cls
+echo This software is protected with the highest level millitary grade encryption technology (lol).
+echo Please enter the password to continue...
+set /p password=Password:
+if %password%==joe goto choosecolor
+if %password%==Joe goto choosecolor
+if %password%==josh goto hijosh
+if %password%==Josh goto hijosh
+goto error1
+
+:error1
+color c
+cls
+echo WARNING! Wrong password... Attempt 1/3... Try again.
+pause
+goto password1
+
+:password1
+color a
+cls
+echo This software is protected with the highest level millitary grade encryption technology.
+echo Please enter the password to continue...
+set /p password=
+if %password%==joe goto choosecolor
+if %password%==Joe goto choosecolor
+goto error2
+
+:error2
+color c
+cls
+echo WARNING! Wrong password... Attempt 2/3... Try again.
+pause
+goto password2
+
+:password2
+color a
+cls
+echo This software is protected with the highest level millitary grade encryption technology.
+echo Please enter the password to continue...
+set /p password=
+if %password%==joe goto choosecolor
+if %password%==Joe goto choosecolor
+goto error3
+
+:error3
+color c
+cls
+echo WARNING! Wrong password... Attempt 3/3......
+pause
+goto shutdown1dt
+
+:hijosh
+cls
+title Josh, you are a little bitch
+color c
+echo Welcome! Just fyi the time is %time%
+echo Josh is a little bitch huehue
+echo =============
+echo.
+echo Who is Josh?
+echo 1. A mop sop
+echo 2. An inbred idiot
+echo 3. I have no idea who the fuck Josh is nor do I care. How the fuck do I get out of this screen?
+echo 4. Oh shit I messed up. I want to change my theme!
+echo =============
+set /p yourmomspussy=
+if %yourmomspussy%==1 goto answerCorrectWUTdt
+if %yourmomspussy%==2 goto answerCorrectWUTdt
+if %yourmomspussy%==3 goto raydt
+if %yourmomspussy%==4 goto choosecolor
+
+:choosecolor
+cls
+color 7
+echo Welcome! What theme color would you like?
+echo 1. Light Theme
+echo 2. Dark Theme
+echo 3. Blue Theme
+echo 4. Red Theme
+echo 5. #PurpleRain
+echo 6. Secret Menu
+echo 7. Log Out
+echo 8. Ghetto Log Out
+set /p welcome=
+if %welcome%==1 goto homelt
+if %welcome%==2 goto homedt
+if %welcome%==3 goto homejosh
+if %welcome%==4 goto homedtr
+if %welcome%==5 goto homedtpr
+if %welcome%==6 goto yourmumgayhahahaha
+if %welcome%==7 goto choosecolor
+if %welcome%==8 goto fuckyoucamyoustinklikeamonkeylol
+
+:fuckyoucamyoustinklikeamonkeylol
+cls
+color 7
+echo You have choosen to log out the 'ghetto' way. Enjoy.
+pause
+cls
+echo We're no strangers to love
+pause
+echo You know the rules and so do I
+pause
+echo A full commitment's what I'm thinking of
+pause
+echo You wouldn't get this from any other guy
+pause
+cls
+color 1
+echo I just wanna tell you how I'm feeling
+pause
+echo Gotta make you understand...
+pause
+cls
+echo Eh, fuck it. See ya
+pause
+goto :finalcountdown
+
+:finalcountdown
+shutdown -s -t 30 -c "Interior crocodile alligator, I drive a Chevrolet movie theater"
+exit
+
+:yourmumgayhahahaha
+cls
+color 7
+echo =================
+echo Welcome to the secret yet non-secret but very secret but not really too secret menu.
+echo At this time, you must be thinking: "What the fuck did I do to deserve this?"
+echo Honestly, I am asking my self that too. Anyway, choose whatever the fuck you want to go to and leave me alone.
+echo Oool, k thanks!
+echo =================
+echo 1. GTAPoliceMods is #1
+echo 2. GTAPoliceMods sucks balls
+echo 3. LCPDFR is #1
+echo 4. You guessed it, GTAPoliceMods
+echo 5. Oh shit, I need to go back to the previous page because I am retarded and have no life like my dad.
+set /p yourmumgayandsmellslikeshit=
+if %yourmumgayandsmellslikeshit%==1 start https://www.gtapolicemods.com/admin
+if %yourmumgayandsmellslikeshit%==2 start https://www.gtapolicemods.com/admin
+if %yourmumgayandsmellslikeshit%==3 start https://www.gtapolicemods.com/admin
+if %yourmumgayandsmellslikeshit%==4 start https://www.gtapolicemods.com/admin
+if %yourmumgayandsmellslikeshit%==5 goto choosecolor
+goto choosecolor
+
+:homedt
+cls
+color 7
+echo Welcome! Just fyi the time is %time%
+echo Josh is a little bitch huehue
+echo =============
+echo.
+echo Who is JOE?
+echo 1. Joe Swanson
+echo 2. Joe Mama
+echo 3. I have no fucking clue who the fuck joe is and ray needs to find something else to do
+echo 4. Oh shit I messed up. I want to change my theme!
+echo =============
+echo.
+set /p op=
+if %op%==1 goto retardAlertLOLdt
+if %op%==2 goto answerCorrectWUTdt
+if %op%==3 goto raydt
+if %op%==4 goto choosecolor
+:retardAlertLOLdt
+cls
+color c
+echo Your fucking retarded. Honestly more than Josh at this point.
+pause
+goto home
+:answerCorrectWUTdt
+cls
+color a
+echo Congrats! You are right! You should just turn off your computer because you are a waste of life.
+pause
+goto home
+:raydt
+cls
+color c
+echo I'm sorry I have to do this to you...
+pause
+goto turnoffpcdt
+:turnoffpcdt
+cls 
+echo Computer now restarting. Once again this was your damn fault. Stupid malaka
+pause
+cls
+echo Ok... So do you take back what you chose?
+echo 1. Yes
+echo 2. No...
+set /p op=
+if %op%==1 goto yourfuckeddt
+if %op%==2 goto yourfuckeddt
+pause
+goto yourfuckeddt
+:yourfuckeddt
+cls
+echo Too bad. Enjoy your computer being turned off!
+pause
+goto shutdown1dt
+:shutdown1dt
+cls
+echo Goodbye!
+shutdown -s -t 120 -c "We're no strangers to love
+You know the rules and so do I
+A full commitment's what I'm thinking of
+You wouldn't get this from any other guy
+I just wanna tell you how I'm feeling
+Gotta make you understand
+Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you
+We've known each other for so long
+Your heart's been aching but you're too shy to say it
+Inside we both know what's been going on
+We know the game and we're gonna play it
+And if you ask me how I'm feeling
+Don't tell me you're too blind to see"
+pause
+goto yourfucked
+
+:homedtr
+cls
+color 47
+echo Welcome! Just fyi the time is %time%
+echo Josh is a little bitch huehue
+echo =============
+echo.
+echo Who is JOE?
+echo 1. Joe Swanson
+echo 2. Joe Mama
+echo 3. I have no fucking clue who the fuck joe is and ray needs to find something else to do
+echo 4. Oh shit I messed up. I want to change my theme!
+echo =============
+echo.
+set /p op=
+if %op%==1 goto retardAlertLOLdtr
+if %op%==2 goto answerCorrectWUTdtr
+if %op%==3 goto raydtr
+if %op%==4 goto choosecolor
+:retardAlertLOLdtr
+cls
+color 47
+echo Your fucking retarded. Honestly more than Josh at this point.
+pause
+goto homedtr
+:answerCorrectWUTdtr
+cls
+color a
+echo Congrats! You are right! You should just turn off your computer because you are a waste of life.
+pause
+goto homedtr
+:raydtr
+cls
+color 47
+echo I'm sorry I have to do this to you...
+pause
+goto turnoffpcdtr
+:turnoffpcdtr
+cls 
+echo Computer now restarting. Once again this was your damn fault. Stupid malaka
+pause
+cls
+echo Ok... So do you take back what you chose?
+echo 1. Yes
+echo 2. No...
+set /p op=
+if %op%==1 goto yourfuckedr
+if %op%==2 goto yourfuckedr
+pause
+goto yourfuckedr
+:yourfuckedr
+cls
+echo Too bad. Enjoy your computer being turned off!
+pause
+goto shutdown1r
+:shutdown1r
+cls
+echo Goodbye!
+shutdown -s -t 120 -c "Stupid Malaka"
+pause
+goto yourfuckedr
+
+:homelt
+cls
+color 70
+echo THIS IS HOME LT
+echo Welcome! Just fyi the time is %time%
+echo Josh is a little bitch huehue
+echo =============
+echo.
+echo Who is JOE?
+echo 1. Joe Swanson
+echo 2. Joe Mama
+echo 3. I have no fucking clue who the fuck joe is and ray needs to find something else to do
+echo 4. Oh shit I messed up. I want to change my theme!
+echo =============
+echo.
+set /p op=
+if %op%==1 goto retardAlertLOLlt
+if %op%==2 goto answerCorrectWUTlt
+if %op%==3 goto ray
+if %op%==4 goto choosecolor
+:retardAlertLOLlt
+cls
+color c
+echo Your fucking retarded. Honestly more than Josh at this point.
+pause
+goto home
+:answerCorrectWUTlt
+cls
+color a
+echo Congrats! You are right! You should just turn off your computer because you are a waste of life.
+pause
+goto home
+:raylt
+cls
+color c
+echo I'm sorry I have to do this to you...
+pause
+goto turnoffpc
+:turnoffpclt
+cls 
+echo Computer now restarting. Once again this was your damn fault. Stupid malaka
+pause
+cls
+echo Ok... So do you take back what you chose?
+echo 1. Yes
+echo 2. No...
+set /p op=
+if %op%==1 goto yourfucked
+if %op%==2 goto yourfucked
+pause
+goto yourfucked
+:yourfuckedlt
+cls
+echo Too bad. Enjoy your computer being turned off!
+pause
+goto shutdown1
+:shutdown1lt
+cls
+echo Goodbye!
+shutdown -s -t 120 -c "Stupid Malaka"
+pause
+goto yourfucked
+
+
+
+:homejosh
+cls
+color 3
+echo Welcome! Just fyi the time is %time%
+echo Josh is a little bitch huehue
+echo =============
+echo.
+echo Who is JOE?
+echo 1. Joe Swanson
+echo 2. Joe Mama
+echo 3. I have no fucking clue who the fuck joe is and ray needs to find something else to do
+echo 4. Oh shit I messed up. I want to change my theme!
+echo =============
+echo.
+set /p op=
+if %op%==1 goto retardAlertLOLjosh
+if %op%==2 goto answerCorrectWUTjosh
+if %op%==3 goto rayjosh
+if %op%==4 goto choosecolor
+:retardAlertLOLjosh
+cls
+color c
+echo Your fucking retarded. Honestly more than Josh at this point.
+pause
+goto homejosh
+:answerCorrectWUTjosh
+cls
+color a
+echo Congrats! You are right! You should just turn off your computer because you are a waste of life.
+pause
+goto homejosh
+:rayjosh
+cls
+color c
+echo I'm sorry I have to do this to you...
+pause
+goto turnoffpcjosh
+:turnoffpcjosh
+cls 
+echo Computer now restarting. Once again this was your damn fault. Stupid malaka
+pause
+cls
+echo Ok... So do you take back what you chose?
+echo 1. Yes
+echo 2. No...
+set /p op=
+if %op%==1 goto yourfuckedjosh
+if %op%==2 goto yourfuckedjosh
+pause
+goto yourfuckedjosh
+:yourfuckedjosh
+cls
+echo Too bad. Enjoy your computer being turned off!
+pause
+goto shutdown1josh
+:shutdown1josh
+cls
+echo Goodbye!
+shutdown -s -t 120 -c "Stupid Malaka"
+pause
+goto yourfuckedjosh
+
+
+:homedtpr
+cls
+color d
+echo Welcome! Just fyi the time is %time%
+echo Josh is a little bitch huehue
+echo =============
+echo.
+echo Who is JOE?
+echo 1. Joe Swanson
+echo 2. Joe Mama
+echo 3. I have no fucking clue who the fuck joe is and ray needs to find something else to do
+echo 4. Oh shit I messed up. I want to change my theme!
+echo =============
+echo.
+set /p op=
+if %op%==1 goto retardAlertLOLdtpr
+if %op%==2 goto answerCorrectWUTdtpr
+if %op%==3 goto raydtpr
+if %op%==4 goto choosecolor
+:retardAlertLOLdtpr
+cls
+color c
+echo Your fucking retarded. Honestly more than Josh at this point.
+pause
+goto homedtpr
+:answerCorrectWUTdtpr
+cls
+color d
+echo Congrats! You are right! You should just turn off your computer because you are a waste of life.
+pause
+goto homedtpr
+:raydtpr
+cls
+color d
+echo I'm sorry I have to do this to you...
+pause
+goto turnoffpcdtpr
+:turnoffpcdtpr
+cls 
+echo Computer now restarting. Once again this was your damn fault. Stupid malaka
+pause
+cls
+echo Ok... So do you take back what you chose?
+echo 1. Yes
+echo 2. No...
+set /p op=
+if %op%==1 goto yourfuckeddtpr
+if %op%==2 goto yourfuckeddtpr
+pause
+goto yourfuckeddtpr
+:yourfuckeddtpr
+cls
+echo Too bad. Enjoy your computer being turned off!
+pause
+goto shutdown1dtpr
+:shutdown1dtpr
+cls
+echo Goodbye!
+shutdown -s -t 120 -c "Stupid Malaka"
+pause
+goto yourfuckeddtpr
+
+
